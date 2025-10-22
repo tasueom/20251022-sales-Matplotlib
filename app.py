47b8aa22e2e278dd95bec9e90244c8f1a129f444
tasueom <tasueom@naver.com>
2025-10-22 16:35:25 +0900

    feat: 실행 시 index.html로 이동

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template as ren, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return ren('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
